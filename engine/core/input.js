c5cc41e49fbb49a463a507bca679f4c2c270ebe0
Vladimir Vukicevic <vladimir@pobox.com>
2012-12-19 11:42:37 -0500

    kill input events if stand_alone is true; this prevents all sorts of brower hotkeys to open the console etc.

--- engine/core/input.js
+++ engine/core/input.js
@@ -1,4 +1,4 @@
-// Copyright 2004-present Facebook. All Rights Reserved.
+
 
 // Licensed under the Apache License, Version 2.0 (the "License"); you may
 // not use this file except in compliance with the License. You may obtain
@@ -185,6 +185,9 @@ var Input = (function() {
     }
 
     function hookEvents(eid) {
+      if (window['stand_alone'])
+        return;
+
       var touch = Browser.mobile;
 
       var el = document.getElementById(eid);
